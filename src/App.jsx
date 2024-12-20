2fa2071b05f0c1299766c2b78a7c232fae926c74
Avinash kumar Mahato <akr34206@gmail.com>
2024-12-20 11:24:23 +0530

    UI of Admin Login and Teacher Login is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import Login from "./pages/Login"
 
 function App() {
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     <Login />
     </>
   )
 }
